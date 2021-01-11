1bb4fa4c3dec0dfd7cbea6339b69a23345df1397
famhy <fahmidouiri@gmail.com>
2021-01-11 18:34:29 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
 
+import "./App.css";
+import './fontawesome-free-5.15.1-web/css/fontawesome.css'
+import "./fontawesome-free-5.15.1-web/css/brands.css"
+import "./fontawesome-free-5.15.1-web/css/solid.css"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg-img">
+
+      </div>
+      <div className="cart">
+        <div className="main">
+          <div className="location">
+            <div className="location-name">
+              Buch<span>arest,RO</span>
+            </div>
+            <div className="location-info">
+              <i class="fas fa-map-marker-alt"></i>
+              <span>12:33</span>
+            </div>
+          </div>
+        </div>
+      <div className="forecast">
+       
+        <div className="today">
+          <div>
+          <div className="temperature">
+              82°
+              <span className="day">Monday</span>
+          </div>
+          </div>
+       
+            <i class="fas fa-cloud-showers-heavy"></i>
+        </div>
+       
+    
+        <div className="forecast-day">
+        <span className="day">Mon</span>
+            <div className="temperature">
+              82°
+            </div>
+            <i class="fas fa-cloud-showers-heavy"></i>
+        </div>
+
+        <div className="forecast-day">
+        <span className="day">Mon</span>
+            <div className="temperature">
+              82°
+            </div>
+            <i class="fas fa-cloud-showers-heavy"></i>
+        </div>
+
+        <div className="forecast-day">
+        <span className="day">Mon</span>
+            <div className="temperature">
+              82°
+            </div>
+            <i class="fas fa-cloud-showers-heavy"></i>
+        </div>
+
+        <div className="forecast-day">
+        <span className="day">Mon</span>
+            <div className="temperature">
+              82°
+            </div>
+            <i class="fas fa-cloud-showers-heavy"></i>
+        </div>
+
+        <div className="forecast-day">
+        <span className="day">Mon</span>
+            <div className="temperature">
+              82°
+            </div>
+            <i class="fas fa-cloud-showers-heavy"></i>
+        </div>
+
+        <div className="forecast-day">
+        <span className="day">Mon</span>
+            <div className="temperature">
+              82°
+            </div>
+            <i class="fas fa-cloud-showers-heavy"></i>
+        </div>
+      
+  
+      </div>
+      </div>
     </div>
   );
 }
